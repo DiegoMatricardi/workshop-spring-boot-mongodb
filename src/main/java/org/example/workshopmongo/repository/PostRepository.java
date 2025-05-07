817a0159a5e54a0766f840e51f8c020c7b105bcb
Diego Matricardi <diegobmatricardi@hotmail.com>
2025-05-07 07:52:30 -0300

    Consulta simples com @Query

--- src/main/java/org/example/workshopmongo/repository/PostRepository.java
+++ src/main/java/org/example/workshopmongo/repository/PostRepository.java
@@ -3,11 +3,16 @@ package org.example.workshopmongo.repository;
 import org.example.workshopmongo.domain.Post;
 import org.example.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> seachByTitle(String text);
+
+
     List<Post> findByTitle(String text);
 }
